828d01bd1d98b5dabfc78a2d1cee9f9a4f83c77f
Alisson Amaral <aalissonamaral98@gmail.com>
2023-01-24 09:25:34 -0300

    inclusao do componente box no storybook e a criacao do mesmo

---
+++ packages/docs/src/stories/Box.stories.tsx
@@ -0,0 +1,17 @@
+import { StoryObj, Meta } from '@storybook/react'
+import { Box, BoxProps } from '@wakanda-ignite-ui/react'
+import React from 'react'
+
+export default {
+  title: 'Surfaces/Box',
+  component: Box,
+  args: {
+    children: (
+      <>
+        <span>Testando o elemento box</span>
+      </>
+    ),
+  },
+} as Meta<BoxProps>
+
+export const Primary: StoryObj<BoxProps> = {}
